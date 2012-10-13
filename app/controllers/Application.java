1a25259cbba2c8c962a6cc192c4903022f556971
Ivan Makarov <vanjamakarov@yahoo.com>
2012-10-13 18:47:38 +0300

    added validation for user creation

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,45 +1,25 @@
 package controllers;
 
 import models.User;
+import play.data.validation.Valid;
+import play.mvc.Before;
 import play.mvc.Controller;
+import play.mvc.With;
 
 import java.util.Date;
 
+@With(Security.class)
+
 public class Application extends Controller {
+	@Before
+	static void setConnectedUser() {
+		if (Security.isConnected()) {
+			User user = User.find("byUsername", Security.connected()).first();
+			renderArgs.put("user", user.username);
+		}
+	}
 
 	public static void index() {
 		render();
 	}
-
-	public static void help() {
-		render();
-	}
-    public static void register() {
-        render();
-    }
-    public static void handleSubmit(
-            String username,
-            String firstname,
-            String lastname,
-            Date birthdate,
-            String password,
-            String email,
-            String about,
-            String country,
-            String city) {
-         User user= new User();
-        user.username=username;
-        user.firstName=firstname;
-        user.lastName=lastname;
-        user.birthdate=birthdate;
-        user.password=password;
-        user.email=email;
-        user.about=about;
-        user.city=city;
-
-
-        // Ok, display the created user
-        render(username, firstname, lastname, birthdate, password, email, about, country, city);
-    }
-
 }
---
+++ app/controllers/Security.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import services.UserService;
+
+
+public class Security extends Secure.Security {
+	static boolean authenticate(String username, String password) {
+		return UserService.connect(username, password) != null;
+	}
+}
