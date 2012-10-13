aa4ff558a9d8d3f435b0729c39880bbee18caade
Tanya <tanya.barakuda@mail.ru>
2012-10-13 10:29:32 +0300

    user registration form added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import models.User;
 import play.mvc.Controller;
+
+import java.util.Date;
 
 public class Application extends Controller {
 
@@ -11,4 +14,32 @@ public class Application extends Controller {
 	public static void help() {
 		render();
 	}
+    public static void register() {
+        render();
+    }
+    public static void handleSubmit(
+            String username,
+            String firstname,
+            String lastname,
+            Date birthdate,
+            String password,
+            String email,
+            String about,
+            String country,
+            String city) {
+         User user= new User();
+        user.username=username;
+        user.firstName=firstname;
+        user.lastName=lastname;
+        user.birthdate=birthdate;
+        user.password=password;
+        user.email=email;
+        user.about=about;
+        user.city=city;
+
+
+        // Ok, display the created user
+        render(username, firstname, lastname, birthdate, password, email, about, country, city);
+    }
+
 }
