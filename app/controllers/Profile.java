bcfb0815c920a1b5df33a8713228196b73648081
Ivan Makarov <vanjamakarov@yahoo.com>
2012-11-14 00:15:36 +0200

    created User profile, refactored some reusable code

---
+++ app/controllers/Profile.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import models.DealAgent;
+import models.UniversalPost;
+import models.User;
+import play.data.validation.Valid;
+import play.mvc.Controller;
+import services.UserService;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: viru
+ * Date: 13.11.12
+ * Time: 22:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class Profile extends Controller {
+
+	public static void index() {
+		User user = User.find("byUsername", session.get("username")).first();
+		List<UniversalPost> createdPosts = UniversalPost.find("byAuthor", user).fetch();
+//		List<UniversalPost> participatedPosts = DealAgent.find("byHelper", user).fetch();
+
+		renderArgs.put("user", user);
+		renderArgs.put("createdPosts", createdPosts);
+		render();
+	}
+
+	public static void edit(@Valid User user) {
+//		if (validation.hasErrors()) {
+//			index();
+//		}
+		if (UserService.updateInfo(user)) {
+			index();
+		} else {
+			render("errors/500.html");
+		}
+	}
+}
